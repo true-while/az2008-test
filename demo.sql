f70ebb111b676ed3cb1e58c2b18d6b8c42bf05bc
true-while <aivanofff@gmail.com>
2025-04-10 13:15:07 -0400

    Fix Issues #2

---
+++ demo.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customer (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone_number VARCHAR(15)
+);
